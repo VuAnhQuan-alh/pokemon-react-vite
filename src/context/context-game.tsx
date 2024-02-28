5032f5fb41d48e8dcab5a8baf115503d0d045db1
VuAnhQuan-alh <quanva2521@gmail.com>
2024-02-28 18:36:10 +0700

    fix: context-game

--- src/context/context-game.tsx
+++ src/context/context-game.tsx
@@ -1,11 +1,23 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
+type IContext = {
+  children: ReactNode;
+};
+type IGameContext = {
+  status: string;
+};
 
-const GameContext = createContext(null);
-export const GameProvider = ({ children }) => {
+const GameContext = createContext<IGameContext | null>(null);
+export const GameProvider = ({ children }: IContext) => {
+  const [state, setState] = useState<IGameContext>({ status: "init" });
   // point của user,
   // trạng thái của game
+  useEffect(() => {
+    setState({ status: "completed" });
+  }, []);
 
   // function cộng điểm
 
-  return <GameContext.Provider value={{}}>{children}</GameContext.Provider>;
+  return (
+    <GameContext.Provider value={{ ...state }}>{children}</GameContext.Provider>
+  );
 };
